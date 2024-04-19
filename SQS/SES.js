a6337704159d65bce0cb89d90a802a260acb0571
Karla J <165040932+Karla-Ju@users.noreply.github.com>
2024-04-19 11:23:44 -0400

    AWS SES Lambda Function

--- SQS/SES.js
+++ SQS/SES.js
@@ -1,1 +1,27 @@
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
+const ses = new SESClient({ region: "us-east-2"});
+
+export const handler = async(event,context) => {
+  const command = new SendEmailCommand({
+    Destination: {
+      ToAddresses: ["Enter your email address."],
+    },
+    Message: {
+      Body: {
+        Text: { Data: "This is a sample email from the AWS SES Lambda"},
+      },
+      Subject: { Data: "Enter the desired email subject."},
+    },
+    Source: "Enter your email address.",
+  });
+  try {
+    let response = await ses.send(command);
+    return response;
+  }
+  catch (error) {
+    console.log(error);
+  }
+  finally {
+  }
+};
